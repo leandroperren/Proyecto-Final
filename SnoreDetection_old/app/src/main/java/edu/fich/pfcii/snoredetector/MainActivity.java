3e136d6aae086aeee49a4a88d547e9362d9da4bf
Matias D. Meurzet <matiasmeurzet@gmail.com>
2016-03-01 20:28:37 -0300

    Se cambio el nombre del paquete Java en Android

--- SnoreDetection_old/app/src/main/java/edu/fich/pfcii/snoredetector/MainActivity.java
+++ SnoreDetection_old/app/src/main/java/edu/fich/pfcii/snoredetector/MainActivity.java
@@ -1,4 +1,4 @@
-package org.example.snoredetection;
+package edu.fich.pfcii.snoredetector;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import org.example.snoredetection.R;
 
 public class MainActivity extends AppCompatActivity {
 
